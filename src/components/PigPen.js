00aaaa8516872cf6843f3e8753c8fce754df2041
Morgan VanYperen <morgvanny@gmail.com>
2018-11-02 14:51:13 -0500

    part 2

--- src/components/PigPen.js
+++ src/components/PigPen.js
@@ -4,11 +4,55 @@ import Filter from './Filter';
 import hogs from '../porkers_data.js';
 
 class PigPen extends Component {
+  state = {
+    orderBy: 'name',
+    filter: false
+  };
+
+  changeOrderBy = e => {
+    this.setState({ orderBy: e.target.value });
+  };
+
+  toggleFilter = () => {
+    this.setState(stateSnapShot => {
+      return { filter: !stateSnapShot.filter };
+    });
+  };
+
+  sortByName(hogs) {
+    return hogs.sort((hag, hbg) => {
+      return hag.name.localeCompare(hbg.name);
+    });
+  }
+
+  sortByWeight(hogs) {
+    return hogs.sort((hag, hbg) => {
+      return (
+        hbg[
+          'weight as a ratio of hog to LG - 24.7 Cu. Ft. French Door Refrigerator with Thru-the-Door Ice and Water'
+        ] -
+        hag[
+          'weight as a ratio of hog to LG - 24.7 Cu. Ft. French Door Refrigerator with Thru-the-Door Ice and Water'
+        ]
+      );
+    });
+  }
+
   render() {
+    const filteredHogs = this.state.filter
+      ? hogs.filter(hog => hog.greased)
+      : [...hogs];
+    const sortedHogs =
+      this.state.orderBy === 'name'
+        ? this.sortByName(filteredHogs)
+        : this.sortByWeight(filteredHogs);
     return (
       <div>
-        <Filter />
-        {hogs.map(hog => {
+        <Filter
+          handleChange={this.changeOrderBy}
+          handleCheck={this.toggleFilter}
+        />
+        {sortedHogs.map(hog => {
           return <PigCard {...hog} key={hog.name} />;
         })}
       </div>
