2fa355096340360a7fb6c6aa70e6cb2ab24490f4
Kim Pham <pham.kim.a@gmail.com>
2018-11-03 12:35:52 -0500

    Done.

--- src/components/App.js
+++ src/components/App.js
@@ -4,15 +4,13 @@ import Nav from './Nav'
 import hogs from '../porkers_data';
 import Index from './Index'
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-          < Nav />
-          < Index hogs={hogs} />
-      </div>
-    )
-  }
+const App = () => {
+  return (
+    <div className="App">
+        < Nav />
+        < Index hogs={hogs} />
+    </div>
+  )
 }
 
 export default App;
